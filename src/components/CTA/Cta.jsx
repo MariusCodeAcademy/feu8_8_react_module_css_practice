dea1cae6ac5b8da9df9eedd2709c1405374bb2c3
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-16 10:53:17 +0300

    cta

--- src/components/CTA/Cta.jsx
+++ src/components/CTA/Cta.jsx
@@ -7,14 +7,14 @@ export default function Cta() {
       <div className={`container ${css.cta}`}>
         <div className='left'>
           <h2 className={css.title}>
-            The fastest way from idea to live site. Period.
+            The fastest way from <br /> idea to live site. Period.
           </h2>
           <p className={css.text}>
             Flex is a Small SaaS Business. Flex isn't a traditional company.
           </p>
         </div>
-        <div className={css.flex}>
-          <Btn>Get started</Btn>
+        <div className={css.right}>
+          <Btn className={css.mr24}>Get started</Btn>
           <Btn secondary>Learn more</Btn>
         </div>
       </div>
--- src/components/UI/btn/Btn.jsx
+++ src/components/UI/btn/Btn.jsx
@@ -2,6 +2,8 @@ import css from './style.module.css';
 
 export default function Btn(props) {
   console.log('css ===', css);
-  const genClasses = props.secondary ? `${css.btn} ${css.gray}` : css.btn;
+  const genClasses = props.secondary
+    ? `${css.btn} ${css.gray} ${props.className}`
+    : `${css.btn} ${props.className}`;
   return <button className={genClasses}>{props.children}</button>;
 }
